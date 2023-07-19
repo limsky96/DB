a51942f6c869e33b0a6b1de5c89d3c841c7fa854
limsky96 <moodforcozy@gmail.com>
2023-07-19 18:04:49 +0900

    db

--- sql50.sql
+++ sql50.sql
@@ -74,3 +74,21 @@ select count(distinct(mgr)) as "Number of Manager" from emp;
 select max(sal)-min(sal) from emp;
 -- 35. 관리자 번호 및 해당 관리자에 속한 사원들의 최저 급여를 출력하라. 단, 관리자가 없는 사원 및 최저 급여가 1000 미만인 그룹은 제외시키고 급여를 기준으로 출력 결과를 내림차순으로 정렬하라.
 select mgr, min(sal) from emp where mgr is not null group by mgr having min(sal)>=1000 order by min(sal);
+-- 36. 부서별로 부서이름, 부서위치, 사원 수 및 평균 급여를 출력하라. 그리고 각각의 컬럼명을 부서명,위치,사원의 수,평균급여로 표시하라.
+select e.deptno as "부서명", d.dname as "부서이름", d.loc as "위치", count(e.empno) as "사원의 수", avg(e.sal) as "평균급여"
+from emp e, dept d where e.deptno=d.deptno group by e.deptno, d.dname, d.loc;
+-- 37. Smith와 동일한 부서에 속한 모든 사원의 이름 및 입사일을 출력하라. 단, Smith는 제외하고 출력하시오
+select ename, to_char(hiredate, 'YYYY-MM-DD') from emp
+where deptno in(select deptno from emp where ename='SMITH') and ename != 'SMITH';
+-- 38. 자신의 급여가 평균 급여보다 많은 모든 사원의 사원 번호, 이름, 급여를 표시하는 질의를 작성하고 급여를 기준으로 결과를 내림차순으로 정렬하라.
+select empno, ename, sal from emp where sal > (select avg(sal) from emp) order by sal desc;
+-- 38-1. 평균 추가 
+select empno, ename, sal, (select avg(sal) from emp) as "평균" from emp
+where sal > (select avg(sal) from emp) order by sal desc;
+-- 39. 이름에 T가 들어가는 사원이 속한 부서에서 근무하는 모든 사원의 사원번호 및 이름을 출력하라.
+select empno, ename from emp where deptno in(select deptno from emp where ename like '%T%');
+-- 40. 부서위치가 Dallas인 모든 사원의 이름,부서번호 및 직위를 출력하라.
+SELECT E.ENAME, E.DEPTNO, E.JOB
+FROM EMP E, DEPT D
+WHERE E.DEPTNO=D.DEPTNO
+AND D.LOC='DALLAS'
